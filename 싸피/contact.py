7f54c01b0b630af03882823befabb19acd3bb7b6
keeeeeey <sseioul@naver.com>
2022-07-31 23:22:09 +0900

    SWEA contact

--- 싸피/contact.py
+++ 싸피/contact.py
@@ -1,5 +1,23 @@
+from collections import deque
+
 def bfs(call_dict, start):
-    pass
+    cnt = 0
+    q = deque()
+    q.append([cnt, start])
+    visited = [start]
+    last = []
+    while q:
+        c, now = q.popleft()
+        if cnt != c:
+            cnt += 1
+        if now in call_dict.keys():
+            for next in call_dict[now]:
+                if next not in visited:
+                    q.append([c + 1, next])
+                    last.append([c, next])
+                    visited.append(next)
+    last.sort(key=lambda x: (-x[0], -x[1]))
+    return last[0][1]
 
 for tc in range(1, 11):
     length, start = map(int, input().split())
