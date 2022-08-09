d92d30229510c298afe13f435dc364e818b3ef95
keeeeeey <sseioul@naver.com>
2022-08-09 16:58:38 +0900

    구간합 투포인터 수정

--- 00.SSAFY_ALGORITHM/수업시간 문제풀이/구간합2.py
+++ 00.SSAFY_ALGORITHM/수업시간 문제풀이/구간합2.py
@@ -4,17 +4,15 @@ for tc in range(1, t + 1):
     arr = list(map(int, input().split()))
 
     lt = 0
-    rt = m - 1
+    rt = m
     temp = 0
-    for i in range(rt + 1):
+    for i in range(rt):
         temp += arr[i]
 
     min_value = temp
     max_value = temp
 
-    while rt < n - 1:
-        rt += 1
-
+    while rt < n:
         temp -= arr[lt]
         temp += arr[rt]
 
@@ -25,6 +23,7 @@ for tc in range(1, t + 1):
             min_value = temp
 
         lt += 1
+        rt += 1
 
     answer = max_value - min_value
     print(f"#{tc} {answer}")
