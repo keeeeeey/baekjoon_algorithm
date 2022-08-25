10d41f60157e0f7594b2818bbdd9ac893c3eee8f
keeeeeey <sseioul@naver.com>
2022-08-25 18:00:06 +0900

    [FIX] 낚시터 자리잡기

--- 00.SSAFY_ALGORITHM/수업시간 문제풀이/낚시터 자리잡기.py
+++ 00.SSAFY_ALGORITHM/수업시간 문제풀이/낚시터 자리잡기.py
@@ -1,7 +1,6 @@
 def permutation(l, temp):
     if l == 3:
-        print(temp)
-
+        fish(temp)
     else:
         for i in range(3):
             if ch[i] == 0:
@@ -11,10 +10,20 @@ def permutation(l, temp):
                 ch[i] = 0
                 temp.pop()
 
+def fish(temp):
+    global answer
+    for t in temp:
+        cnt = 0
+
+
 t = int(input())
 for tc in range(1, t + 1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(3)]
 
     ch = [0] * 3
+    answer = 0
+    place = [0] * (n + 1)
+
     permutation(0, [])
+
