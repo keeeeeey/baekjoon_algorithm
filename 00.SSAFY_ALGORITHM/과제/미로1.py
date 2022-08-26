914214304a271940de1f1fae97034f808942e5f0
keeeeeey <sseioul@naver.com>
2022-08-26 21:14:38 +0900

    [20220825 과제]

--- 00.SSAFY_ALGORITHM/과제/미로1.py
+++ 00.SSAFY_ALGORITHM/과제/미로1.py
@@ -1,17 +1,15 @@
-from collections import deque
-
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def bfs(maze, start, end):
-    q = deque([start])
+def bfs(start):
+    q = [start]
     while q:
-        x, y = q.popleft()
+        x, y = q.pop(0)
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < 16 and 0 <= ny < 16:
-                if nx == end[0] and ny == end[1]:
+                if maze[nx][ny] == 3:
                     return 1
 
                 if maze[nx][ny] == 0:
@@ -24,13 +22,10 @@ for _ in range(10):
     maze = list(list(map(int, input().strip())) for _ in range(16))
 
     start = [0, 0]
-    end = [0, 0]
 
     for i in range(16):
         for j in range(16):
             if maze[i][j] == 2:
                 start = [i, j]
-            if maze[i][j] == 3:
-                end = [i, j]
 
-    print(f"#{tc} {bfs(maze, start, end)}")
+    print(f"#{tc} {bfs(start)}")
