fcfc524cf90239b166168679ba59c6ea3f7be553
keeeeeey <sseioul@naver.com>
2022-09-22 17:58:06 +0900

    [FIX] 파이프 옮기기 수정

--- 00.SSAFY_ALGORITHM/a형 역량테스트 대비 문제/파이프 옮기기 1.py
+++ 00.SSAFY_ALGORITHM/a형 역량테스트 대비 문제/파이프 옮기기 1.py
@@ -1,46 +1,31 @@
-import sys
-input = sys.stdin.readline
-
-def direction(head, tail):
-    dx, dy, l = [], [], 0
-    if head[0] == tail[0]:
-        dx = [0, 1]
-        dy = [1, 1]
-        l = 2
-    elif head[1] == tail[1]:
-        dx = [1, 1]
-        dy = [0, 1]
-        l = 2
-    elif head[0] != tail[0] and head[1] != tail[1]:
-        dx = [1, 0, 1]
-        dy = [0, 1, 1]
-        l = 3
-
-    return dx, dy, l
-
-def dfs(head, tail):
+dx = [1, 0, 1]
+dy = [0, 1, 1]
+def dfs(x, y, idx):
     global answer
-    dx, dy, l = direction(head, tail)
 
-    for d in range(l):
-        nx = head[0] + dx[d]
-        ny = head[1] + dy[d]
+    if x == n - 1 and y == n - 1:
+        answer += 1
+        return
+
+    for d in range(3):
+        if d == idx:
+            continue
+
+        nx = x + dx[d]
+        ny = y + dy[d]
         if 0 <= nx < n and 0 <= ny < n:
             if dx[d] == dy[d]:
                 if room[nx][ny] == 0 and room[nx - 1][ny] == 0 and room[nx][ny - 1] == 0:
-                    if nx == n - 1 and ny == n - 1:
-                        answer += 1
-                        return
-                    dfs([nx, ny], head)
-            else:
+                    dfs(nx, ny, 3)
+            elif dx[d] == 1:
                 if room[nx][ny] == 0:
-                    if nx == n - 1 and ny == n - 1:
-                        answer += 1
-                        return
-                    dfs([nx, ny], head)
+                    dfs(nx, ny, 1)
+            elif dy[d] == 1:
+                if room[nx][ny] == 0:
+                    dfs(nx, ny, 0)
 
 n = int(input())
 room = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
-dfs([0, 1], [0, 0])
+dfs(0, 1, 0)
 print(answer)
