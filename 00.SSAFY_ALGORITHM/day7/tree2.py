1765386706e43a037a68adc4e627b5d39d475491
keeeeeey <sseioul@naver.com>
2022-09-26 18:00:55 +0900

    [FEAT] 20220926 수업 내용

--- 00.SSAFY_ALGORITHM/day7/tree2.py
+++ 00.SSAFY_ALGORITHM/day7/tree2.py
@@ -1,9 +1,32 @@
 def pre(n):
     if n <= size:
-        print(tree[n])
+        pre_arr.append(tree[n])
         pre(2 * n)
         pre(2 * n + 1)
 
-tree = [0, "A", "B", "C", "D", "E", "F"]
+def inorder(n):
+    if n <= size:
+        inorder(2 * n)
+        in_arr.append(tree[n])
+        inorder(2 * n + 1)
+
+def postorder(n):
+    if n <= size:
+        postorder(2 * n)
+        postorder(2 * n + 1)
+        post_arr.append(tree[n])
+
+tree = [0, 1, 2, 3]
 size = len(tree) - 1
+pre_arr = []
+in_arr = []
+post_arr = []
 pre(1)
+inorder(1)
+postorder(1)
+print(pre_arr)
+print(in_arr)
+print(post_arr)
+answer = []
+for i in range(3):
+    answer.append(max)
