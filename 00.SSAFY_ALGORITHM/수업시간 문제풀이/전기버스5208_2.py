04b79b8f0ba03f818568479e51d8b954302785ba
keeeeeey <sseioul@naver.com>
2022-09-27 17:48:07 +0900

    [FEAT] 20220927 전기 버스 풀이

--- 00.SSAFY_ALGORITHM/수업시간 문제풀이/전기버스5208_2.py
+++ 00.SSAFY_ALGORITHM/수업시간 문제풀이/전기버스5208_2.py
@@ -1,42 +1,23 @@
-def subset(l, v):
-    global answer
-    if v >= answer:
-        return
-
-    if l == n - 1:
-        if arrive():
-            answer = v
-        return
-
-    ch[l] = 1
-    subset(l + 1, v + 1)
-    ch[l] = 0
-    subset(l + 1, v)
-
-def arrive():
-    now = 0
-    battery = batteries[0]
-    while battery:
-        now += 1
-        battery -= 1
-
-        if ch[now] == 1:
-            battery = batteries[now]
-            if now + batteries[now] >= n - 1:
-                now = n - 1
-                break
-
-    if now == n - 1:
-        return True
-    else:
-        return False
-
 t = int(input())
+
+def backtracking(idx, cnt):
+    global min_v
+
+    if cnt >= min_v:
+        return
+
+    if idx == n - 1:
+        min_v = min(min_v, cnt)
+        return
+
+    for i in range(1, bus_stop[idx] + 1):
+        backtracking(idx + i, cnt + 1)
+
 for tc in range(1, t + 1):
-    arr = list(map(int, input().split()))
-    n = arr[0]
-    batteries = arr[1:n]
-    ch = [0] * (n - 1)
-    answer = n - 2
-    subset(1, 0)
-    print(f"#{tc} {answer}")
+    line = list(map(int, input().split()))
+    n = line[0]
+    bus_stop = line[1:] + [0]
+    min_v = 101
+
+    backtracking(0, -1)
+    print(f"#{tc} {min_v}")
