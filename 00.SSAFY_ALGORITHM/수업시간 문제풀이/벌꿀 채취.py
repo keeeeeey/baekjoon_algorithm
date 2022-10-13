d78cc898d389e76db8a0602f700d1918450ef270
keeeeeey <sseioul@naver.com>
2022-10-13 22:10:49 +0900

    a형 대비 문제 풀이

--- 00.SSAFY_ALGORITHM/수업시간 문제풀이/벌꿀 채취.py
+++ 00.SSAFY_ALGORITHM/수업시간 문제풀이/벌꿀 채취.py
@@ -1,7 +1,53 @@
+def perm(l, x, y):
+    global honey
+    if l == m:
+        temp = 0
+        bottle = c
+        for i in range(m):
+            if honey_box[x][y + ch[i]] <= bottle:
+                bottle -= honey_box[x][y + ch[i]]
+                temp += honey_box[x][y + ch[i]] ** 2
+        honey = max(honey, temp)
+    else:
+        for i in range(m):
+            if i not in ch:
+                ch[l] = i
+                perm(l + 1, x, y)
+                ch[l] = -1
+
+def check(x, y):
+    flag = True
+    for i in range(m):
+        if visited[x][y + i] == 1:
+            flag = False
+            break
+    return flag
+
 t = int(input())
 for tc in range(1, t + 1):
     n, m, c = map(int, input().split())
     honey_box = [list(map(int, input().split())) for _ in range(n)]
     answer = 0
+    honey_max = [[0] * (n - m + 1) for _ in range(n)]
+    for i in range(n):
+        for j in range(n - m + 1):
+            honey = 0
+            ch = [-1] * m
+            perm(0, i, j)
+            honey_max[i][j] = honey
+
+    visited = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n - m + 1):
+            for z in range(m):
+                visited[i][j + z] = 1
+
+            for k in range(i, n):
+                for s in range(n - m + 1):
+                    if check(k, s):
+                        answer = max(answer, honey_max[i][j] + honey_max[k][s])
+
+            for z in range(m):
+                visited[i][j + z] = 0
 
     print(f"#{tc} {answer}")
--- 00.SSAFY_ALGORITHM/수업시간 문제풀이/핀볼 게임.py
+++ 00.SSAFY_ALGORITHM/수업시간 문제풀이/핀볼 게임.py
@@ -1,54 +1,18 @@
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def game_start(x, y, d, cnt):
-    global max_score
-    nx = x + dx[d]
-    ny = y + dy[d]
-    if 0 <= nx < n and 0 <= ny < n:
-        if arr[x][y] == -1:
-            max_score = max(max_score, cnt + 1)
-            return
-        elif arr[nx][ny] == 0:
-            if nx == 0 or nx == n - 1 or ny == 0 or ny == n - 1:
-                game_start(nx, ny, (d + 2) % 4, cnt + 1)
-            else:
-                game_start(nx, ny, d, cnt + 1)
-        elif arr[nx][ny] == 1:
-            if d == 3:
-                game_start(nx, ny, 0, cnt + 1)
-            elif d == 2:
-                game_start(nx, ny, 1, cnt + 1)
-            else:
-                game_start(nx, ny, (d + 2) % 4, cnt + 1)
-        elif arr[nx][ny] == 2:
-            if d == 3:
-                game_start(nx, ny, 2, cnt + 1)
-            elif d == 0:
-                game_start(nx, ny, 1, cnt + 1)
-            else:
-                game_start(nx, ny, (d + 2) % 4, cnt + 1)
-        elif arr[nx][ny] == 3:
-            if d == 1:
-                game_start(nx, ny, 2, cnt + 1)
-            elif d == 0:
-                game_start(nx, ny, 3, cnt + 1)
-            else:
-                game_start(nx, ny, (d + 2) % 4, cnt + 1)
-        elif arr[nx][ny] == 4:
-            if d == 1:
-                game_start(nx, ny, 0, cnt + 1)
-            elif d == 2:
-                game_start(nx, ny, 3, cnt + 1)
-            else:
-                game_start(nx, ny, (d + 2) % 4, cnt + 1)
-        elif arr[nx][ny] == 5:
-            game_start(nx, ny, (d + 2) % 4, cnt + 1)
-        else:
-            for i in range(2):
-                if wormhole[arr[nx][ny] - 6][i][0] == nx and wormhole[arr[nx][ny] - 6][i][1] == ny:
-                    continue
-                game_start(wormhole[arr[nx][ny] - 6][i][0], wormhole[arr[nx][ny] - 6][i][1], d, cnt + 1)
+def game_start(i, j, d):
+    stack = []
+    stack.append((i, j, d))
+    while stack:
+        x, y, d = stack.pop()
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        if nx == i and ny == j:
+            break
+
+        
 
 t = int(input())
 for tc in range(1, t + 1):
@@ -73,6 +37,6 @@ for tc in range(1, t + 1):
     for i in range(n):
         for j in range(n):
             for d in range(4):
-                game_start(i, j, d, 0)
+                game_start(i, j, d)
 
     print(f"#{tc} {max_score}")
